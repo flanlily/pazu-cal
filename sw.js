047b74ec7c07910e0d8d829cdb8171c81fa28253
flanlily <cien_flanlily@outlook.jp>
2025-07-10 13:50:59 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('app-cache').then(function(cache) {
+      return cache.addAll([
+        './',
+        './index.html',
+        './style.css',
+        './dungeonData.json'
+      ]);
+    })
+  );
+});
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
